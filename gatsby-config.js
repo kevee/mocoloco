51435da74e28e34327eec893b940e0a9f795cac4
Kevin Miller <kevin@csumb.edu>
2019-07-19 11:42:20 -0300

    again

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,10 +16,10 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `mocoloco`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mocoloco`,
+        short_name: `mocoloco`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -27,8 +27,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/style/typography',
+      },
+    },
   ],
 }
