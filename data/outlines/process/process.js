d19ebb858f91f83554f78dcd5ba0fe6567efc400
Kevin Miller <kevin@csumb.edu>
2019-07-26 11:31:19 -0300

    Organized outlines

--- data/outlines/process/process.js
+++ data/outlines/process/process.js
@@ -5,15 +5,15 @@ const input = JSON.parse(fs.readFileSync('in.json'))
 const features = {}
 
 input.features.forEach(feature => {
-  if (typeof features[feature.properties.current_id] === 'undefined') {
-    features[feature.properties.current_id] = []
+  if (typeof features[feature.properties.FIRE_DIST] === 'undefined') {
+    features[feature.properties.FIRE_DIST] = []
   }
-  features[feature.properties.current_id].push(feature)
+  features[feature.properties.FIRE_DIST].push(feature)
 })
 
 Object.keys(features).forEach(name => {
   fs.writeFileSync(
-    name,
+    name.replace('/', '-'),
     JSON.stringify({
       type: 'FeatureCollection',
       features: features[name],
