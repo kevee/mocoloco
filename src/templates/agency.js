6dc2025f577c97eba150e3d5b7d9515560e78d97
Kevin Miller <kevin@csumb.edu>
2019-08-01 14:11:23 -0300

    Added map to agency view

--- src/templates/agency.js
+++ src/templates/agency.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Layout from '../components/layouts/default'
+import { Container } from '../components/common/container'
+import PageHeader from '../components/common/page-title'
+import { Flex, Box } from '@rebass/grid/emotion'
+import { Map, GeoJSON, TileLayer } from 'react-leaflet'
 
 const AgencyPage = ({ pageContext }) => {
   const { agency } = pageContext
   return (
     <Layout title="mocoloco">
-      <h1>{agency.name}</h1>
+      <Container>
+        <PageHeader>{agency.name}</PageHeader>
+        <Flex>
+          <Box width={[3 / 4]}></Box>
+          <Box width={[1 / 4]}>
+            {agency.outline && <AgencyMap outline={agency.outline} />}
+          </Box>
+        </Flex>
+      </Container>
     </Layout>
   )
 }
+
+const AgencyMap = ({ outline }) => (
+  <Map
+    style={{ width: '100%', height: 'calc(100vh - 18px)' }}
+    center={[36.3530149, -121.6947624]}
+    zoom={9}
+  >
+    <TileLayer
+      url="https://api.mapbox.com/styles/v1/keveemiller/cjyra1vuc794c1cp458r449wt/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2V2ZWVtaWxsZXIiLCJhIjoiY2p5cjl0aWRvMDZmYjNjcHUzeDVwOHN3MCJ9.E-R7THevDHSXUosHYYQJwQ"
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+    <GeoJSON data={JSON.parse(outline.outline)} />
+  </Map>
+)
+
 export default AgencyPage
