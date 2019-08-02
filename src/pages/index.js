7d819ca44b63a3ad96c9712a3966652f39e1ff6c
Kevin Miller <kevin@csumb.edu>
2019-08-02 19:56:19 -0300

    Added  search to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,16 @@ import { TextContainer } from '../components/common/container'
 import { LeadParagraph } from '../components/common/type'
 import styled from '@emotion/styled'
 import { Flex, Box } from '@rebass/grid/dist/emotion'
-import { FormSubmit, TextInput } from '../components/common/forms'
-import { navigate } from 'gatsby'
+import { TextInput } from '../components/common/forms'
+import { Link } from 'gatsby'
+import ListUnstyled from '../components/common/list-unstyled'
 
 const HomeContainer = styled.div`
   margin-top: 5rem;
 `
 
 const Index = ({ data }) => {
+  const [searchResults, setSearchResults] = useState([])
   const [searchQuery, setSearchQuery] = useState('')
   return (
     <Layout title="mocoloco">
@@ -23,24 +25,60 @@ const Index = ({ data }) => {
                 Every government agency and service in Monterey County, right
                 here.
               </LeadParagraph>
-              <form
-                onSubmit={event => {
-                  event.preventDefault()
-                  navigate(`/search/?q=${searchQuery}`)
-                }}
-              >
+              <form>
                 <TextInput
                   type="text"
                   aria-label="Search"
                   placeholder="Monterey County"
                   onChange={event => {
-                    setSearchQuery(event.target.value)
+                    const searchQuery = event.target.value.trim().toLowerCase()
+                    setSearchQuery(searchQuery)
+                    if (!searchQuery.length) {
+                      setSearchResults([])
+                      return
+                    }
+                    const results = []
+                    data.allContentfulAgency.nodes.forEach(agency => {
+                      if (agency.name.toLowerCase().search(searchQuery) > -1) {
+                        results.push(agency)
+                      }
+                    })
+                    results.sort((a, b) => {
+                      return a.name.toLowerCase().trim() <
+                        b.name.toLowerCase().trim()
+                        ? -1
+                        : 1
+                    })
+                    setSearchResults(results)
                   }}
                 ></TextInput>
-                <FormSubmit value="Search" />
               </form>
             </Box>
-            <Box width={[1 / 3]} ml={[3]}></Box>
+            <Box width={[1 / 3]} ml={[3]}>
+              {searchResults && searchResults.length > 0 && (
+                <ListUnstyled>
+                  {searchResults.map((result, index) => (
+                    <>
+                      {index < 5 && (
+                        <li>
+                          <Link to={`/agency/${result.slug}`}>
+                            {result.name}
+                          </Link>
+                        </li>
+                      )}
+                    </>
+                  ))}
+                  {searchResults.length > 5 && (
+                    <li>
+                      ...and{' '}
+                      <Link to={`/search/?q=${searchQuery}`}>
+                        {searchResults.length - 5} more
+                      </Link>
+                    </li>
+                  )}
+                </ListUnstyled>
+              )}
+            </Box>
           </Flex>
           <LeadParagraph></LeadParagraph>
         </HomeContainer>
@@ -50,3 +88,23 @@ const Index = ({ data }) => {
 }
 
 export default Index
+
+export const query = graphql`
+  {
+    allContentfulAgency(sort: { fields: [name] }) {
+      nodes {
+        name
+        slug
+        type {
+          id
+          name
+          slug
+        }
+        issues {
+          id
+          name
+        }
+      }
+    }
+  }
+`
